667fe90b60345650ce7514b5e2b477bc6791505a
Boehm <boehmk@pllimskhpc3.mskcc.org>
2024-02-10 13:38:40 -0500

    add shell scripts

---
+++ feat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# activate conda environment
+source /home/boehmk/.bashrc
+conda activate mussel
+
+# navigate to directory
+cd /gpfs/mskmind_ess/boehmk/python_bin/mussel
+
+python extract_features.py --model quilt --save_dir "/gpfs/mskmind_ess/boehmk/scratch" --slide_file_path "/gpfs/mskmind_emc/data_large/pathology/BR_20-226/slides/${1}.svs" --patch_file_path "/gpfs/mskmind_ess/boehmk/scratch/patches/${1}.h5"
