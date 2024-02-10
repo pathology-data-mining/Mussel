cf1e3e73481c6e2f8654223a207501d23b5d8e0f
Kevin <8082119+kmboehm@users.noreply.github.com>
2024-02-10 14:09:51 -0500

    paralllel

--- feat.sh
+++ feat.sh
@@ -7,4 +7,9 @@ conda activate mussel
 # navigate to directory
 cd /gpfs/mskmind_ess/boehmk/python_bin/mussel
 
-python extract_features.py --model quilt --save_dir "/gpfs/mskmind_ess/boehmk/scratch" --slide_file_path "/gpfs/mskmind_emc/data_large/pathology/BR_20-226/slides/${1}.svs" --patch_file_path "/gpfs/mskmind_ess/boehmk/scratch/patches/${1}.h5"
+python extract_features.py \
+--model quilt \
+--save_dir "/gpfs/mskmind_ess/boehmk/scratch" \
+--slide_file_path "/gpfs/mskmind_emc/data_large/pathology/BR_20-226/slides/${1}.svs" \
+--patch_file_path "/gpfs/mskmind_ess/boehmk/scratch/patches/${1}.h5" \
+--gpus 0 1 2 3
