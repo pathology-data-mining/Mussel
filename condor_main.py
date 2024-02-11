2969edb025f6b015c371e8f96003d29d49fcdb3d
Kevin <8082119+kmboehm@users.noreply.github.com>
2024-02-11 17:46:00 -0500

    condor

--- condor_main.py
+++ condor_main.py
@@ -21,46 +21,73 @@ def filter_to_pending(all_args):
         status = check_reef_status(temp_args)
         if status[field_to_check]:
             print(f"{image_id} has already undergone {all_args['command']} in reef")
+        elif not status['slide_exists']:
+            print(f"{image_id} source .svs file not found")
         else:
             image_ids_to_process.append(image_id)
     all_args['image_id'] = image_ids_to_process
     if len(all_args['image_id']) == 0:
         print(f"No images to process for {all_args['command']}")
         exit()
-    return all_args
+    return all_args, len(all_args['image_id'])
 
-def submit_condor_jobs(mussel_params, n_jobs):
-    job = htcondor.Submit({"executable": "data/patch_and_cache.sh",
-                           "arguments": "$(ProcId) {mpp} {patch_size} {step_size} {norm}".format(**mussel_params),
-                           "universe": "vanilla",
-                           "request_gpus": 0,
-                           "request_memory": "10GB",
-                           "request_cpus": 8,
-                           "output": "outputs/preprocessing/condor_logs/output.out",
-                           "log": "outputs/preprocessing/condor_logs/log.log",
-                           "error": "outputs/preprocessing/condor_logs/err.err"})
+def format_args(all_args):
+    arg_str = "$(ProcID) "
+    parser.add_argument("command", help="tessellate, featurize, annotate, cache")
+    parser.add_argument('--image_id', nargs='+', type=str, help='image id')
+    parser.add_argument('--reef_dir', type=str, help='location of mussel reef', default='/gpfs/mskmind_ess/pdm/reef')
+    parser.add_argument('--mpp', type=float, help='microns per pixel', default=1.0)
+    parser.add_argument('--patch_size', type=int, help='patch size', default=224)
+    parser.add_argument('--step_size', type=int, help='step size', default=896)
+
+    # Add featurize arguments
+    featurize_group = parser.add_argument_group('featurize', 'featurize options')
+    featurize_group.add_argument('--model_name', type=str, help='model', default='quilt')
+    featurize_group.add_argument('--gpus', nargs="+", type=int, default=[0])
+    featurize_group.add_argument('--batch_size', type=int, default=64)
+
+    # Add annotate arguments
+    annotate_group = parser.add_argument_group('annotate', 'annotate options')
+    annotate_group.add_argument('--interrogate', action='store_true', help='interrogate')
+
+    # Add cache arguments
+    cache_group = parser.add_argument_group('cache', 'cache options')
+    cache_group.add_argument("--limit_to_class", type=str, default=None, help="limit to class")
+
+def submit_condor_jobs(all_args):
+    all_args['image_ids_formatted'] = " ".join(all_args['image_id'])
+    sub = htcondor.Submit()
+    sub['executable'] = 'condor_mussel.sh'
+    sub['universe'] = 'vanilla'
+    sub['request_gpus'] = 0
+    sub['request_memory'] = '10GB'
+    sub['request_cpus'] = 8
+    sub['output'] = 'scratch/condor_output.out'
+    sub['log'] = 'scratch/condor_log.log'
+    sub['error'] = 'scratch/condor_err.err'
+    arguments = []
+    for image_id in all_args['image_id']:
+        temp_args = all_args.copy()
+        temp_args['image_id'] = image_id
+        arguments.append(f"{temp_args['command']} --image_id {image_id} --reef_dir {temp_args['reef_dir']} --mpp {temp_args['mpp']} --patch_size {temp_args['patch_size']} --step_size {temp_args['step_size']} --model_name {temp_args['model_name']} --gpus {temp_args['gpus']} --batch_size {temp_args['batch_size']} --interrogate {temp_args['interrogate']} --limit_to_class {temp_args['limit_to_class']}")
     schedd = htcondor.Schedd()
-    submit_result = schedd.submit(job, count=n_jobs)
-    cluster_id = submit_result.cluster()
+    with schedd.transaction() as txn:
+        result = sub.queue_with_itemdata(txn, 1, arguments)
+    cluster_id = result.cluster()
     print(f"Submitted HTCondor cluster with ID: {cluster_id}")
     return cluster_id    
 
 
 if __name__ == "__main__":
-    all_args = parse_args()
-    assert len(all_args['image_id']) > 1, "use main.py for single image_id"
-    all_args = filter_to_pending(all_args)
-    for image_id in all_args['image_id']:
-        args = all_args.copy()
-        args['image_id'] = image_id
-        main(args)
-
-    # cluster_id = submit_condor_jobs(MUSSEL_PARAMS, len(SLIDES_TO_CACHE))
+    ALL_ARGS = parse_args()
+    assert len(ALL_ARGS['image_id']) > 1, "use main.py for single image_id"
+    ALL_ARGS, N_JOBS = filter_to_pending(ALL_ARGS)
+    CLUSTER_ID = submit_condor_jobs(ALL_ARGS)
     
     # # wait until done
-    # schedd = htcondor.Schedd()
-    # cluster_ads = schedd.query(f"ClusterId == {cluster_id}")
-    # while len(cluster_ads) > 0:
-    #     print(f"Waiting for {len(cluster_ads)} jobs to finish...")
-    #     time.sleep(30)
-    #     cluster_ads = schedd.query(f"ClusterId == {cluster_id}")
+    SCHEDD = htcondor.Schedd()
+    CLUSTER_ADS = SCHEDD.query(f"ClusterId == {CLUSTER_ID}")
+    while len(CLUSTER_ADS) > 0:
+        print(f"Waiting for {len(CLUSTER_ADS)} jobs to finish...")
+        time.sleep(10)
+        CLUSTER_ADS = SCHEDD.query(f"ClusterId == {CLUSTER_ID}")
