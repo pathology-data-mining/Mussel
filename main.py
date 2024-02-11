7469becd172cc1920620d8657552b0b8cb39e3bb
Kevin <8082119+kmboehm@users.noreply.github.com>
2024-02-11 18:31:53 -0500

    fix

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def parse_args():
     args = parser.parse_args()
     args = vars(args)
 
-    if args['image_id'].contains('.txt'):
+    if len(args['image_id']) == 1 and args['image_id'][0].contains('.txt'):
         with open(args['image_id'], 'r') as f:
             args['image_id'] = f.readlines()
 
