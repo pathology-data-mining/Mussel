cdba7cea7685f34a2ab0234fb42a87243047f53d
Kevin <8082119+kmboehm@users.noreply.github.com>
2024-02-11 12:55:52 -0500

    class

--- annotate_for_tumor.py
+++ annotate_for_tumor.py
@@ -39,11 +39,17 @@ PARSER.add_argument("--class_json_path", type=str)
 PARSER.add_argument("--output_path", type=str)
 ARGS = PARSER.parse_args()
 
+# load precomputed embeddings
 CLASS_DICT = load_classes(ARGS.class_json_path)
 CLASS_EMB = load_class_embs(ARGS.class_json_path)  # N_classes 512
 
 SLIDE_EMB = torch.load(ARGS.slide_emb_path)  # N_tiles 512
 
+# zero-shot classification
 COS_SIM = torch.nn.functional.cosine_similarity(SLIDE_EMB.unsqueeze(1), CLASS_EMB.unsqueeze(0), dim=2)  # N_tiles N_classes
 DF = pd.DataFrame(COS_SIM.numpy(), columns=[CLASS_DICT[i] for i in range(len(CLASS_DICT))])
 print(DF)
+DF.to_csv(ARGS.output_path, index=False)
+
+DF['class'] = DF.idxmax(axis=1).map(CLASS_DICT)
+print(DF['class'].value_counts())
