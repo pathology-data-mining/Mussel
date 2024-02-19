aacdd409822567d62f3d686ccddd18e3028613e4
Raymond Lim <raylim@mm.st>
2024-02-19 16:47:40 -0500

    improvement: 1748 scuba UI improvements

--- scuba/app.py
+++ scuba/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template, url_for
 from cached_search import CachedSearch
 import matplotlib.pyplot as plt
 import io
@@ -11,7 +11,7 @@ PARSER = argparse.ArgumentParser()
 PARSER.add_argument("device", help="Device to run on (cpu or cuda:{0,1,2,3})")
 ARGS = PARSER.parse_args()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 QUILT = load_quilt()
 IMG_EMB_DIR = "/gpfs/mskmind_ess/pdm/reef/1.0_224_896_None/feats/quilt/pt"
@@ -23,12 +23,13 @@ CS = CachedSearch(QUILT, IMG_EMB_DIR, PATCH_DIR, SLIDE_DIR, OUTPUT_DIR, ARGS.dev
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    image_data_urls = []
+    query = ""
     if request.method == 'POST':
         query = request.form.get('query')
         image_ids, tile_indices = CS.search(query)
         images = [CS.load_image(image_id, tile_index) for image_id, tile_index in zip(image_ids, tile_indices)]
         # Convert images to data URLs so they can be displayed in the HTML
-        image_data_urls = []
         for img in images:
             if img is None:
                 continue
@@ -36,49 +37,6 @@ def home():
             img.save(img_io, 'PNG')
             img_io.seek(0)
             image_data_urls.append('data:image/png;base64,' + urllib.parse.quote(base64.b64encode(img_io.read())))
-        return render_template_string('''
-            <style>
-                body { font-family: Arial, sans-serif; margin: 0; padding: 0; background: white; color: black; display: flex; justify-content: center; align-items: center; height: 100vh; }
-                form { display: flex; justify-content: center; align-items: center; }
-                input[type="text"] { padding: 10px; font-size: 1.5em; }
-                .image-container { display: flex; flex-wrap: wrap; justify-content: center; }
-                .image-container img { margin: 10px; }
-            </style>
-            <form method="POST">
-                <input name="query" type="text" placeholder="Search" autofocus>
-            </form>
-            <h1> {{ query }} </h1>
-            <div class="image-container">
-                {% for image_data_url in image_data_urls %}
-                    <img src="{{ image_data_url }}">
-                {% endfor %}
-            </div>
-        ''', image_data_urls=image_data_urls, query=query)
-    else:
-        return '''
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    margin: 0;
-                    padding: 0;
-                    background-image: "background-image: url('/static/reef.jpg')";
-                    background-repeat: no-repeat;
-                    background-size: cover;
-                    color: black;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                }
-                form { display: flex; justify-content: center; align-items: center; }
-                input[type="text"] { padding: 10px; font-size: 1.5em; }
-            </style>
-            <div class="container">
-                <form method="POST">
-                    <input name="query" type="text" placeholder="Search" autofocus>
-                </form>
-            </div>
-        '''
-
+    return render_template('index.html', image_data_urls=image_data_urls, num_slides=CS.len, query=query)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
