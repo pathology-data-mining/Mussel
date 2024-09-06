608fb8bb52f0ed66a0f031daeeafd15f901026e1
Raymond Lim <raylim@mm.st>
2024-09-06 12:12:20 -0400

    fix: tessellate test

--- tests/mussel/cli/test_tessellate.py
+++ tests/mussel/cli/test_tessellate.py
@@ -7,17 +7,14 @@ from mussel.cli.tessellate import TessellateConfig, SegConfig, PatchConfig
 def test_tessellate(tmp_path):
     slide_path = "tests/testdata/948176.svs"
     patch_h5_path = tmp_path / "test.h5"
-    stitch_path = tmp_path / "test.jpg"
     seg_config = SegConfig(segment_threshold=0)
     cfg = TessellateConfig(
         slide_path=slide_path,
         output_h5_path=patch_h5_path,
-        stitch_jpeg_path=stitch_path,
         seg_config=seg_config,
         num_workers=1,
     )
     mussel.cli.tessellate.main(OmegaConf.create(cfg))
     assert os.path.exists(patch_h5_path)
-    assert os.path.exists(stitch_path)
 
 
