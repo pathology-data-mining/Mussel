52d8a20cabf3aa88696e81586b691d7c48644193
Raymond Lim <raylim@mm.st>
2024-09-24 17:14:32 -0400

    feat: linear probing benchmark

---
+++ mussel/cli/linear_probe_benchmark.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import hydra
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from loguru import logger
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING, OmegaConf
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, f1_score
+
+
+@dataclass
+class LinearProbeBenchmarkConfig:
+    features_annotation_parquet_path: str = MISSING
+    annotation_percent_filter_threshold: float = 0.5
+    annotation_mean_threshold: float = 0.5
+
+cs = ConfigStore.instance()
+cs.store(name="linear_probe_benchmark_config", node=LinearProbeBenchmarkConfig)
+
+@hydra.main(version_base=None, config_path=".", config_name="linear_probe_benchmark_config")
+def main(cfg: LinearProbeBenchmarkConfig):
+    df = gpd.read_parquet(cfg.features_annotation_parquet_path)
+
+    df_filtered = df.query(f"annotation_count > {cfg.annotation_percent_filter_threshold} * patch_size * patch_size")
+
+    df_filtered['y'] = (df_filtered.annotation_mean > cfg.annotation_mean_threshold).astype(int)
+
+    slide_ids = df_filtered['slide_id'].unique()
+
+    train_ids, test_ids = train_test_split(slide_ids, test_size=0.2, random_state=42)
+    train_ids, val_ids = train_test_split(train_ids, test_size=0.125, random_state=42)  # 0.25 * 0.8 = 0.2
+
+    train_df = df_filtered[df_filtered['slide_id'].isin(train_ids)]
+    val_df = df_filtered[df_filtered['slide_id'].isin(val_ids)]
+    test_df = df_filtered[df_filtered['slide_id'].isin(test_ids)]
+
+    X_train = train_df.filter(regex='feature_').values
+    y_train = train_df['y'].values
+
+    X_val = val_df.filter(regex='feature_').values
+    y_val = val_df['y'].values
+
+    X_test = test_df.filter(regex='feature_').values
+    y_test = test_df['y'].values
+
+    C_values = [0.01, 0.1, 1, 10, 100]
+
+    best_f1 = 0
+    best_model = None
+    best_C = None
+
+    for C in C_values:
+        # Initialize logistic regression model
+        clf = LogisticRegression(C=C, max_iter=5000, solver='lbfgs')
+        # Train the model
+        clf.fit(X_train, y_train)
+        # Validation predictions
+        y_val_pred = clf.predict(X_val)
+        val_f1 = f1_score(y_val, y_val_pred, average='weighted')
+        print(f"C_value : {C} \t F1_score: {val_f1}")
+        if val_f1 > best_f1:
+            best_f1 = val_f1
+            best_model = clf
+            best_C = C
---
+++ mussel/cli/merge_annotation_features.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import hydra
+import h5py
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from loguru import logger
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING, OmegaConf
+from PIL import Image
+from shapely.geometry import box
+
+Image.MAX_IMAGE_PIXELS = 100000000000
+
+
+@dataclass
+class MergeAnnotationFeaturesConfig:
+    features_h5_path: str = MISSING
+    annotation_bmp_path: str = MISSING
+    output_parquet_path: str = MISSING
+    slide_id: Optional[str] = None
+    class_mapping: Optional[dict] = None
+
+cs = ConfigStore.instance()
+cs.store(name="merge_annotation_features_config", node=MergeAnnotationFeaturesConfig)
+
+@hydra.main(version_base=None, config_path=".", config_name="merge_annotation_features_config")
+def main(cfg: MergeAnnotationFeaturesConfig):
+    cfg.class_mapping = { 1 : 1, 2 : 1, 3 : 1, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0, 11 : 0, }
+    with open(cfg.features_h5_path, 'rb') as f:
+        logger.info(f"Reading features from {cfg.features_h5_path}...")
+        tiles_h5 = h5py.File(f, 'r')
+        patch_size = tiles_h5['coords'].attrs.get("patch_size")
+        df = pd.DataFrame(tiles_h5['coords'], columns=['i', 'j'])
+
+        tiles = [box(row.i, row.j, row.i + patch_size, row.j + patch_size) for row in df.itertuples(index=False)]
+        tiles_gdf = gpd.GeoDataFrame(
+            pd.DataFrame(tiles_h5['features']).add_prefix('feature_', axis=1),
+            geometry=tiles)
+        tiles_gdf["patch_size"] = patch_size
+        logger.info(f"Loaded {len(tiles_gdf)} tiles")
+
+    logger.info(f"Reading annotations from {cfg.annotation_bmp_path}...")
+    img_arr = np.array(Image.open(cfg.annotation_bmp_path))
+    ind = np.nonzero(img_arr)
+    df = pd.DataFrame(np.transpose(ind), columns = ['i', 'j'])
+    df = df.assign(annotation=img_arr[ind])
+    if cfg.class_mapping is not None:
+        df.annotation = df.annotation.replace(cfg.class_mapping)
+    #df.annotation = pd.Categorical(df.annotation).rename_categories(CATEGORIES)
+    ann_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['i'], df['j']))
+    logger.info(f"Loaded {len(ann_gdf)} annotations pixels")
+
+    logger.info("Joining tiles and annotations...")
+    mgdf = tiles_gdf.sjoin(ann_gdf, how='inner')
+    gb = mgdf.reset_index().groupby('index')
+    ann_count = gb.size()
+    ann_mean = gb.annotation.mean()
+    logger.info(f"Found {len(ann_mean)} tiles with annotations")
+
+    ann_tiles_gdf = tiles_gdf.iloc[ann_mean.index,:]
+    ann_tiles_gdf = ann_tiles_gdf.assign(annotation_count=ann_count, annotation_mean=ann_mean)
+
+    logger.info(f"Writing merged results to {cfg.output_parquet_path}")
+    if cfg.slide_id is not None:
+        ann_tiles_gdf = ann_tiles_gdf.assign(slide_id=cfg.slide_id)
+
+    ann_tiles_gdf.to_parquet(cfg.output_parquet_path)
+
+if __name__ == "__main__":
+    main()
+
