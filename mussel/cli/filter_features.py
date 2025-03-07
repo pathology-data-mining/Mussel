cf76a767a7cf78f8834772011e894b1fd8ea7005
Raymond Lim <raylim@mm.st>
2025-03-07 15:12:29 -0500

    fix: filter features update

--- mussel/cli/filter_features.py
+++ mussel/cli/filter_features.py
@@ -1,10 +1,12 @@
 import os
 import pickle
 from dataclasses import dataclass, field
+from typing import Optional
 
 import h5py
 import hydra
 import numpy as np
+import torch
 from hydra.core.config_store import ConfigStore
 from loguru import logger
 from omegaconf import MISSING, OmegaConf
@@ -15,7 +17,9 @@ from mussel.utils.file import save_hdf5
 @dataclass
 class FilterFeaturesConfig:
     features_h5_path: str = MISSING
+    features_pt_path: Optional[str] = None
     output_h5_path: str = MISSING
+    output_pt_path: str = MISSING
     classifier_pkl: str = MISSING
     classifier_threshold: float = 0.75
 
@@ -23,23 +27,39 @@ class FilterFeaturesConfig:
 cs = ConfigStore.instance()
 cs.store(name="filter_features_config", node=FilterFeaturesConfig)
 
+
 @hydra.main(version_base=None, config_path=".", config_name="filter_features_config")
 def main(
     cfg: FilterFeaturesConfig,
 ):
     logger.info(f"loading model pkl {cfg.classifier_pkl}")
-    with open(cfg.classifier_pkl, 'rb') as f:
+    with open(cfg.classifier_pkl, "rb") as f:
         clf = pickle.load(f)
 
-    with h5py.File(cfg.features_h5_path, 'r') as features_h5:
+    with h5py.File(cfg.features_h5_path, "r") as features_h5:
+        if cfg.features_pt_path:
+            features = torch.load(cfg.features_pt_path)
+        else:
+            features = np.array(features_h5["features"])
+            features = torch.Tensor(features)
         logger.info("Predicting probabilities...")
-        inclusion_mask = clf.predict_proba(features_h5['features'])[:, 1] > cfg.classifier_threshold
-        logger.info(f"{sum(inclusion_mask)} tiles above {cfg.classifier_threshold} threshold")
-        features = features_h5['features'][inclusion_mask]
-        coords = features_h5['coords'][inclusion_mask]
+        inclusion_mask = clf.predict_proba(features)[:, 1] > cfg.classifier_threshold
+        logger.info(
+            f"{sum(inclusion_mask)} tiles above {cfg.classifier_threshold} threshold"
+        )
+        features = features[inclusion_mask]
+        coords = features_h5["coords"][inclusion_mask]
 
-    logger.info(f"Saving to {cfg.output_h5_path}")
-    save_hdf5(cfg.output_h5_path, {"features": features, "coords": coords}, attr_h5_path=cfg.features_h5_path, mode='w')
+        logger.info(f"Saving to {cfg.output_h5_path}")
+        save_hdf5(
+            cfg.output_h5_path,
+            {"coords": coords},
+            attr_h5_path=cfg.features_h5_path,
+            mode="w",
+        )
+
+        torch.save(features, cfg.output_pt_path)
+
 
 if __name__ == "__main__":
     main()
