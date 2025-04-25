baac054d5009d615aa10f707ea9553c67e5c30e0
Raymond Lim <raylim@mm.st>
2025-04-25 12:27:21 -0400

    chore: isort and black

--- mussel/cli/filter_features2.py
+++ mussel/cli/filter_features2.py
@@ -1,7 +1,6 @@
 import os
 import pickle
 from dataclasses import dataclass, field
-from typing import Optional
 
 import h5py
 import hydra
@@ -16,10 +15,9 @@ from mussel.utils.file import save_hdf5
 
 @dataclass
 class FilterFeaturesConfig:
-    features_h5_path: str = MISSING
-    features_pt_path: Optional[str] = None
+    features_pt_path: str = MISSING
+    tiles_h5_path: str = MISSING
     output_h5_path: str = MISSING
-    output_pt_path: str = MISSING
     classifier_pkl: str = MISSING
     classifier_threshold: float = 0.75
 
@@ -36,29 +34,24 @@ def main(
     with open(cfg.classifier_pkl, "rb") as f:
         clf = pickle.load(f)
 
-    with h5py.File(cfg.features_h5_path, "r") as features_h5:
-        if cfg.features_pt_path:
-            features = torch.load(cfg.features_pt_path)
-        else:
-            features = np.array(features_h5["features"])
-            features = torch.Tensor(features)
+    features = torch.load(cfg.features_pt_path)
+    with h5py.File(cfg.tiles_h5_path, "r") as tiles_h5:
         logger.info("Predicting probabilities...")
         inclusion_mask = clf.predict_proba(features)[:, 1] > cfg.classifier_threshold
         logger.info(
             f"{sum(inclusion_mask)} tiles above {cfg.classifier_threshold} threshold"
         )
         features = features[inclusion_mask]
-        coords = features_h5["coords"][inclusion_mask]
+        coords = tiles_h5["coords"][inclusion_mask]
 
-        logger.info(f"Saving to {cfg.output_h5_path}")
-        save_hdf5(
-            cfg.output_h5_path,
-            {"coords": coords},
-            attr_h5_path=cfg.features_h5_path,
-            mode="w",
-        )
-
-        torch.save(features, cfg.output_pt_path)
+    logger.info(f"Saving to {cfg.output_h5_path}")
+    features = features.cpu().numpy()
+    save_hdf5(
+        cfg.output_h5_path,
+        {"features": features, "coords": coords},
+        attr_h5_path=cfg.tiles_h5_path,
+        mode="w",
+    )
 
 
 if __name__ == "__main__":
--- mussel/cli/merge_annotation_features.py
+++ mussel/cli/merge_annotation_features.py
@@ -24,46 +24,56 @@ class MergeAnnotationFeaturesConfig:
     slide_id: Optional[str] = None
     class_mapping_yaml_path: Optional[str] = None
 
+
 cs = ConfigStore.instance()
 cs.store(name="merge_annotation_features_config", node=MergeAnnotationFeaturesConfig)
 
-@hydra.main(version_base=None, config_path=".", config_name="merge_annotation_features_config")
-def main(cfg: MergeAnnotationFeaturesConfig):
-    with open(cfg.features_h5_path, 'rb') as f:
-        logger.info(f"Reading features from {cfg.features_h5_path}...")
-        tiles_h5 = h5py.File(f, 'r')
-        patch_size = tiles_h5['coords'].attrs.get("patch_size")
-        df = pd.DataFrame(tiles_h5['coords'], columns=['i', 'j'])
 
-        tiles = [box(row.i, row.j, row.i + patch_size, row.j + patch_size) for row in df.itertuples(index=False)]
+@hydra.main(
+    version_base=None, config_path=".", config_name="merge_annotation_features_config"
+)
+def main(cfg: MergeAnnotationFeaturesConfig):
+    with open(cfg.features_h5_path, "rb") as f:
+        logger.info(f"Reading features from {cfg.features_h5_path}...")
+        tiles_h5 = h5py.File(f, "r")
+        patch_size = tiles_h5["coords"].attrs.get("patch_size")
+        df = pd.DataFrame(tiles_h5["coords"], columns=["i", "j"])
+
+        tiles = [
+            box(row.i, row.j, row.i + patch_size, row.j + patch_size)
+            for row in df.itertuples(index=False)
+        ]
         tiles_gdf = gpd.GeoDataFrame(
-            pd.DataFrame(tiles_h5['features']).add_prefix('feature_', axis=1),
-            geometry=tiles)
+            pd.DataFrame(tiles_h5["features"]).add_prefix("feature_", axis=1),
+            geometry=tiles,
+        )
         tiles_gdf["patch_size"] = patch_size
         logger.info(f"Loaded {len(tiles_gdf)} tiles")
 
     logger.info(f"Reading annotations from {cfg.annotation_bmp_path}...")
     img_arr = np.array(Image.open(cfg.annotation_bmp_path))
     ind = np.nonzero(img_arr)
-    df = pd.DataFrame(np.transpose(ind), columns = ['j', 'i'])
+    df = pd.DataFrame(np.transpose(ind), columns=["j", "i"])
     df = df.assign(annotation=img_arr[ind])
     if cfg.class_mapping_yaml_path is not None:
-        with open(cfg.class_mapping_yaml_path, 'r') as f:
+        with open(cfg.class_mapping_yaml_path, "r") as f:
             class_mapping = yaml.safe_load(f)
         df.annotation = df.annotation.replace(class_mapping)
-    #df.annotation = pd.Categorical(df.annotation).rename_categories(CATEGORIES)
-    ann_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['i'], df['j']))
+    # df.annotation = pd.Categorical(df.annotation).rename_categories(CATEGORIES)
+    ann_gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["i"], df["j"]))
     logger.info(f"Loaded {len(ann_gdf)} annotations pixels")
 
     logger.info("Joining tiles and annotations...")
-    mgdf = tiles_gdf.sjoin(ann_gdf, how='inner', predicate='within')
-    gb = mgdf.reset_index().groupby('index')
+    mgdf = tiles_gdf.sjoin(ann_gdf, how="inner", predicate="within")
+    gb = mgdf.reset_index().groupby("index")
     ann_count = gb.size()
     ann_mean = gb.annotation.mean()
     logger.info(f"Found {len(ann_mean)} tiles with annotations")
 
-    ann_tiles_gdf = tiles_gdf.iloc[ann_mean.index,:]
-    ann_tiles_gdf = ann_tiles_gdf.assign(annotation_count=ann_count, annotation_mean=ann_mean)
+    ann_tiles_gdf = tiles_gdf.iloc[ann_mean.index, :]
+    ann_tiles_gdf = ann_tiles_gdf.assign(
+        annotation_count=ann_count, annotation_mean=ann_mean
+    )
 
     logger.info(f"Writing merged results to {cfg.output_parquet_path}")
     if cfg.slide_id is not None:
@@ -71,6 +81,6 @@ def main(cfg: MergeAnnotationFeaturesConfig):
 
     ann_tiles_gdf.to_parquet(cfg.output_parquet_path)
 
+
 if __name__ == "__main__":
     main()
-
