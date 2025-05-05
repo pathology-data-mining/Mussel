9dff5fcb29370ed6af7c1d7784ef285257657e9e
Raymond Lim <raylim@mm.st>
2025-05-05 14:11:42 -0400

    tests: fix tessellation test

--- tests/mussel/cli/test_tessellate.py
+++ tests/mussel/cli/test_tessellate.py
@@ -2,15 +2,17 @@ import os
 from omegaconf import OmegaConf
 
 import mussel.cli.tessellate
-from mussel.cli.tessellate import TessellateConfig
+from mussel.cli.tessellate import TessellateConfig, SegConfig
 
 def test_tessellate(tmp_path):
     slide_path = "tests/testdata/948176.svs"
     patch_h5_path = tmp_path / "test.h5"
+    seg_config = SegConfig(segment_threshold=0)
     cfg = TessellateConfig(
         slide_path=slide_path,
         output_h5_path=patch_h5_path,
         segment_threshold=0,
+        seg_config=seg_config,
         num_workers=1,
     )
     mussel.cli.tessellate.main(OmegaConf.create(cfg))
