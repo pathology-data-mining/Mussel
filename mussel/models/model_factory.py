f9abb04d64f538d45610de333c0f6f63716cd804
Raymond Lim <raylim@mm.st>
2025-08-22 15:15:27 -0400

    feat: conch and virchow models

--- mussel/models/model_factory.py
+++ mussel/models/model_factory.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Callable, List
+from pathlib import Path
+import json
 
 import open_clip
 import timm
@@ -10,7 +12,12 @@ from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from timm.layers import SwiGLUPacked
 from torchvision import transforms
+from conch.open_clip_custom import load_checkpoint, CoCa, OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
+from conch.open_clip_custom.factory import read_state_dict
+from conch.open_clip_custom.transform import image_transform
+from huggingface_hub import hf_hub_download
 
+CFG_DIR = Path(__file__).parent / 'configs'
 
 class ModelType(Enum):
     def __init__(self, id, code, hf_path):
@@ -25,6 +32,9 @@ class ModelType(Enum):
     OPTIMUS = 5, "optimus", "hf-hub:bioptimus/H-optimus-0"
     CLIP = 6, "clip", "hf-hub:wisdomik/QuiltNet-B-16-PMB"
     GOOGLEPATH = 7, "googlepath", "google/path-foundation"
+    CONCH = 9, "conch", "hf-hub:MahmoodLab/CONCH"
+    CONCH1_5 = 10, "conch1_5", "hf-hub:MahmoodLab/conchv1_5"
+    VIRCHOW2 = 11, "virchow2", "hf-hub:paige-ai/Virchow2"
 
 
 class Model:
@@ -124,6 +134,83 @@ class TorchModel(Model):
                 return self.obj(x).cpu()
 
         return model_fun
+
+class ConchModel(TorchModel):
+
+    def __init__(
+        self,
+        model_path,
+        model_obj=None,
+        use_gpu: bool = True,
+        gpu_device_id: int | List[int] | None = None,
+    ):
+        model_cfg_path = CFG_DIR / "conch_ViT-B-16.json"
+        with open(model_cfg_path, 'r') as f:
+            model_cfg = json.load(f)
+
+        _ = model_cfg.pop('custom_text', None)
+        model_obj = CoCa(**model_cfg)
+
+        if model_path.startswith("hf-hub:"): 
+            _ = hf_hub_download(model_path[len("hf-hub:"):], 
+                                filename="meta.yaml")
+            model_path = hf_hub_download(model_path[len("hf-hub:"):], 
+                                              filename="pytorch_model.bin")
+
+        load_checkpoint(model_obj, model_path)
+
+        model_obj.visual.image_mean = OPENAI_DATASET_MEAN
+        model_obj.visual.image_std = OPENAI_DATASET_STD
+
+        super().__init__(model_obj, use_gpu, gpu_device_id)
+
+
+    def get_preprocessing_fun(self) -> Callable:
+        image_mean = getattr(self.obj.visual, 'image_mean', None)
+        image_std = getattr(self.obj.visual, 'image_std', None)
+
+        return image_transform(
+            self.obj.visual.image_size,
+            mean=image_mean,
+            std=image_std,
+        )
+
+    def get_model_fun(self) -> Callable:
+        def model_fun(x):
+            with (
+                torch.no_grad(),
+                torch.inference_mode(),
+                torch.autocast(device_type=self.device.type, dtype=torch.float16),
+            ):
+                x = x.to(self.device, non_blocking=True)
+                return self.obj.encode_image(x).cpu()
+
+        return model_fun
+
+class Conch15Model(ConchModel):
+    def __init__(
+        self,
+        model_path,
+        model_obj=None,
+        use_gpu: bool = True,
+        gpu_device_id: int | List[int] | None = None,
+    ):
+        model_cfg_path = CFG_DIR / "conch_ViT-B-16.json"
+        with open(model_cfg_path, 'r') as f:
+            model_cfg = json.load(f)
+
+        _ = model_cfg.pop('custom_text', None)
+        model_obj = CoCa(**model_cfg)
+        if model_path.startswith("hf-hub:"): 
+            _ = hf_hub_download(model_path[len("hf-hub:"):], 
+                                filename="meta.yaml")
+            model_path = hf_hub_download(model_path[len("hf-hub:"):], 
+                                         filename="pytorch_model_vision.bin")
+
+        state_dict = read_state_dict(model_path)
+        model_obj.load_state_dict(state_dict, strict=False)
+
+        TorchModel.__init__(self, model_obj, use_gpu, gpu_device_id)
 
 
 class GigapathTorchModel(TorchModel):
@@ -327,6 +414,27 @@ class VirchowModelFactory(ModelFactory):
     ):
         return VirchowTorchModel(model_path, model_obj, use_gpu, gpu_device_id)
 
+@register_model_factory(ModelType.VIRCHOW2)
+class Virchow2ModelFactory(ModelFactory):
+    def get_model(
+        self, model_path=None, model_obj=None, use_gpu=True, gpu_device_id=None
+    ):
+        return VirchowTorchModel(model_path, model_obj, use_gpu, gpu_device_id)
+
+@register_model_factory(ModelType.CONCH)
+class ConchModelFactory(ModelFactory):
+    def get_model(
+        self, model_path=None, model_obj=None, use_gpu=True, gpu_device_id=None
+    ):
+        return ConchModel(model_path, model_obj, use_gpu, gpu_device_id)
+
+
+@register_model_factory(ModelType.CONCH1_5)
+class Conch15ModelFactory(ModelFactory):
+    def get_model(
+        self, model_path=None, model_obj=None, use_gpu=True, gpu_device_id=None
+    ):
+        return Conch15Model(model_path, model_obj, use_gpu, gpu_device_id)
 
 @register_model_factory(ModelType.OPTIMUS)
 class OptimusModelFactory(ModelFactory):
